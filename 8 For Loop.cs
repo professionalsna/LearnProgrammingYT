4812f55904ed444e7db92d131ae42ad2892b4d1e
Naveed Ahmed <professionalsna@gmail.com>
2023-01-20 12:44:46 +0500

    For Loop

---
+++ 8 For Loop.cs
@@ -0,0 +1,17 @@
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("####################################");
+Console.WriteLine("Welcome Back to The Programming");
+Console.WriteLine("####################################");
+
+//Loop
+//What is Loop? and Why We Use it
+//Example of Loop
+
+
+for (int i = 0; i < 1000; i++)
+{
+    //loop statement
+    Console.WriteLine(i.ToString() + ": Hello World");
+}
+Console.WriteLine("Loop Ended");
+Console.ReadLine();
